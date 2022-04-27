e1eebdf516acef6def970df41341835428f8477c
Omkar Katkar <89191550+omkarkatkar525@users.noreply.github.com>
2022-04-27 16:11:09 +0530

    Update p107_sentiment_analysis_deployment.py

--- p107_sentiment_analysis_deployment.py
+++ p107_sentiment_analysis_deployment.py
@@ -33,7 +33,7 @@ def predict_note_authentication(message):
     #print(my_prediction)
     return render_template('result.html',prediction = my_prediction)
 
-@app.route('/main',button=['POST'])
+@app.route('/main',methods=['Predict'])
 def main():
  st.title("Sentiment analysis")
  html_temp = """
