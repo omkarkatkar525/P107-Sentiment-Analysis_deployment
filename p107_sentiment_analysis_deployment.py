0e42a467b5274095c77d43279937f2c9dd7836e6
Omkar Katkar <89191550+omkarkatkar525@users.noreply.github.com>
2022-05-04 11:36:37 +0530

    Update p107_sentiment_analysis_deployment.py

--- p107_sentiment_analysis_deployment.py
+++ p107_sentiment_analysis_deployment.py
@@ -43,11 +43,11 @@ def probability(message):
     data_1=[message]
     vect_1= cv.transform(data_1).toarray()
     pred_test = clf.predict_proba(vect_1)
-    a=print("Negative Review",pred_test[:1,0:1]) 
-    b=("Neutral Review",pred_test[:1,1:2])
-    c=("Positive Review",pred_test[:1,2:3])
-    print(a)
-    return a,b,c
+    a=pred_test[:1,0:1]
+    b=pred_test[:1,1:2]
+    c=pred_test[:1,2:3]
+   
+    return print(a)
 
 
    
