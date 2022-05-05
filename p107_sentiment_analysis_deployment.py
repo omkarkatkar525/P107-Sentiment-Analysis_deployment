3b48660081f52176e6ac4abb84d6395092ecc8d1
Omkar Katkar <89191550+omkarkatkar525@users.noreply.github.com>
2022-05-05 07:28:21 -0700

    Update p107_sentiment_analysis_deployment.py

--- p107_sentiment_analysis_deployment.py
+++ p107_sentiment_analysis_deployment.py
@@ -65,7 +65,7 @@ def proba(message):
     e=st.write(f"Probability of Neutral Review : {b}")
     f=st.write(f"Probability of Positive Review : {c}")
     g=(d,e,f)
-    
+    g = {'Negative Review': [d], 'Neutral Review': [e],Positive Review:[f]} 
     return st.bar_chart(g)
 
 def main():
